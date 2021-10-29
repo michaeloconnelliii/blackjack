550ac8e52e5f8b74b3f16aaa455e7081ac9ca8c4
Michael O'Connell <michaeloconnelliii@gmail.com>
2021-10-29 00:54:15 -0700

    api calls and setting class members works effectively.

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+class Player {
+    constructor(money) {
+        this.money = money;
+    }
+
+    getMoney() {
+        return this.money;
+    }
+}
+
+class Deck {
+    constructor(deckCount) {
+        this.deckCount = deckCount;
+    }
+       
+    async initDeck() {
+        let deckApiPath = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=${this.deckCount}`;
+        let result = await axios.get(deckApiPath);
+        this.deckId = result.data.deck_id;
+        this.cardsRemaining = result.data.remaining;
+    }
+
+    async shuffle() {
+        let deckApiPath = `https://deckofcardsapi.com/api/deck/${this.deckId}/shuffle/`;
+        let result = await axios.get(deckApiPath);
+        if(!result.data.success) {
+            console.error('Deck failed to shuffle.');
+        }
+        this.cardsRemaining = result.data.remaining;
+    }
+    
+}
+
+let test = new Deck(2);
+
 // Close front page on play a round button
 let playBtn = document.getElementById('play-btn');
 let frontPage = document.getElementById('front-page');
