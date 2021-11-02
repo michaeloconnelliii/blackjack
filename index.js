e289c7b32843f01f216a4dd6ccb42473c947371f
Michael O'Connell <michaeloconnelliii@gmail.com>
2021-11-01 20:22:02 -0700

    method to display the appropriate number of chips is finished.

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ class Player {
     constructor(money) {
         this.money = money;
     }
-
-    getMoney() {
-        return this.money;
-    }
 }
 
 class Deck {
@@ -52,8 +48,13 @@ const playModal = document.getElementById('play-modal');
 const bank = document.getElementById('bank-modal');
 const playerAmt = document.getElementById('player-amt');
 const cardAmt = document.getElementById('card-amt');
+const onesChips = document.getElementById('one-chips-container');
+const twentyFivesChips = document.getElementById('twenty-five-chips-container');
+const fiftyChips = document.getElementById('fifty-chips-container');
+const oneHundredChips = document.getElementById('hundred-chips-container');
+const fiveHundredChips = document.getElementById('five-hundred-chips-container');
 
-const startingMoney = 2000;
+const startingMoney = 5;
 const startingDecks = 2;
 
 let player = new Player(startingMoney);
@@ -67,7 +68,36 @@ function displayCardNum(cardNum) {
 
 // Displays appropriate number of chips. For example, if a user only has $4, 4 $1 chips will be displayed and no others 
 function displayChips() {
+    chipAmtToContainer = {
+        1: onesChips,
+        25: twentyFivesChips,
+        50: fiftyChips,
+        100: oneHundredChips,
+        500: fiveHundredChips
+    };
 
+    const maxDisplayedChips = 5;
+
+    for(amt in chipAmtToContainer) {
+        let chipAmt = Math.floor(player.money / amt);
+        const chipContainer = chipAmtToContainer[amt];
+        const chipElements = chipContainer.children;
+
+        chipAmt = (chipAmt >= maxDisplayedChips ? maxDisplayedChips : chipAmt);
+        
+        // Make correct number of chips visible
+        for(let i = 0; i < chipAmt; i++) {
+            chipElements[i].classList.remove('hidden');
+        }
+
+        // Make the rest of chips hidden
+        for(let i = chipAmt; i < maxDisplayedChips; i++) {
+            chipElements[i].classList.add('hidden');
+        }
+
+        // Hide the container if there aren't any chips visible
+        chipAmt <= 0 ? chipContainer.classList.add('hidden') : chipContainer.classList.remove('hidden');
+    }
 }
 
 /* TODO
